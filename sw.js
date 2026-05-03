5320dc924e1d80d6c1e625c192663f7b29a2cd9f
IceEmerald <114658093+IceEmerald@users.noreply.github.com>
2026-05-03 14:10:12 +0000

    EmeraldNetwork Update 26.03.05

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "emeraldnetwork.emeraldcore.offlineHandler.0.0.2";
+const CACHE_NAME = "emeraldnetwork.emeraldcore.serviceWorker.v1";
 const OFFLINE_URL = "./offline.html";
 
 const ASSETS_TO_CACHE = [
