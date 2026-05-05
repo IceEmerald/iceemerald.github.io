08b8818abe247f44520b40e0293509d0359b3797
IceEmerald <114658093+IceEmerald@users.noreply.github.com>
2026-05-05 19:55:26 +0700

    ENCore 4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "emeraldnetwork.emeraldcore.serviceWorker.v1.0.1";
+const CACHE_NAME = "emeraldnetwork.emeraldcore.serviceWorker.v1.0.2";
 const OFFLINE_URL = "./offline.html";
 
 const ASSETS_TO_CACHE = [
